097f8ccc10c3c61c5d6c9df6f696512a70cfbb6a
Jose Martinez <joariel17@gmail.com>
2024-09-06 00:39:56 -0300

    api fetch, by city

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,55 @@
 import { useEffect, useState } from 'react'
-import { api, Weather } from './api'
+import { api } from './api'
+import { CityAlt, WeatherAlt } from './types'
+
+const CITIES: Record<string, CityAlt> = {
+  bsar: {
+    id: 'bs-ar',
+    name: 'Bs. As.',
+    lat: -34.6157129,
+    lon: -58.5981051,
+  },
+  cordoba: {
+    id: 'cordoba',
+    name: 'Cordoba',
+    lat: -32.47298,
+    lon: -68.0972914,
+  },
+  jujuy: {
+    id: 'jujuy',
+    name: 'Jujuy',
+    lat: -23.1751717,
+    lon: -68.3224852,
+  },
+  tucuman: {
+    id: 'tucuman',
+    name: 'Tucuman',
+    lat: -26.8328589,
+    lon: -65.2225937,
+  },
+  misiones: {
+    id: 'misiones',
+    name: 'Misiones',
+    lat: -26.8240001,
+    lon: -56.1672424,
+  },
+}
 
 function App() {
   const [status, setStatus] = useState<'pending' | 'success'>('pending')
-  const [weather, setWeather] = useState<Weather | null>(null)
+  const [weather, setWeather] = useState<WeatherAlt | null>(null)
+  const [city, setCity] = useState<CityAlt>(CITIES['bsar'])
+
+  const handleChangeCity = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setCity(CITIES[e.target.value as keyof typeof CITIES])
+  }
 
   useEffect(() => {
-    api.weather.fetch(0, 0).then((data) => {
+    api.weather.fetch(city).then((data) => {
       setWeather(data)
       setStatus('success')
     })
-  }, [])
+  }, [city])
 
   if (status === 'pending') {
     return <div>Cargando...</div>
@@ -19,13 +58,22 @@ function App() {
     return <div>La ciudad no existe o no hay datos del clima</div>
   }
 
+  console.log(weather.forecast.length)
+
   return (
     <main>
+      <select onChange={handleChangeCity} value={city.id}>
+        {Object.values(CITIES).map((city) => (
+          <option key={city.id} value={city.id}>
+            {city.name}
+          </option>
+        ))}
+      </select>
       <h1>{weather.city.name}</h1>
       <ul>
-        {weather.forecast.map((item, index) => (
+        {weather.forecast.map((forecast, index) => (
           <li key={index}>
-            Min:{item.min}, Max:{item.max}
+            {forecast.date}: Min:{forecast.min} °C, Max:{forecast.max} °C
           </li>
         ))}
       </ul>
--- src/api.ts
+++ src/api.ts
@@ -1,196 +1,33 @@
-export type Weather = {
-  city: {
-    id: number
-    name: string
-  }
-  forecast: {
-    min: number
-    max: number
-  }[]
-}
+import { CityAlt, RawWeather, WeatherAlt } from './types'
 
 export const api = {
   weather: {
-    fetch: async (lat: number, lon: number): Promise<Weather> => {
-      const response = {
-        cod: '200',
-        message: 0,
-        cnt: 40,
-        list: [
-          {
-            dt: 1661871600,
-            main: {
-              temp: 296.76,
-              feels_like: 296.98,
-              temp_min: 296.76,
-              temp_max: 297.87,
-              pressure: 1015,
-              sea_level: 1015,
-              grnd_level: 933,
-              humidity: 69,
-              temp_kf: -1.11,
-            },
-            weather: [
-              {
-                id: 500,
-                main: 'Rain',
-                description: 'light rain',
-                icon: '10d',
-              },
-            ],
-            clouds: {
-              all: 100,
-            },
-            wind: {
-              speed: 0.62,
-              deg: 349,
-              gust: 1.18,
-            },
-            visibility: 10000,
-            pop: 0.32,
-            rain: {
-              '3h': 0.26,
-            },
-            sys: {
-              pod: 'd',
-            },
-            dt_txt: '2022-08-30 15:00:00',
-          },
-          {
-            dt: 1661882400,
-            main: {
-              temp: 295.45,
-              feels_like: 295.59,
-              temp_min: 292.84,
-              temp_max: 295.45,
-              pressure: 1015,
-              sea_level: 1015,
-              grnd_level: 931,
-              humidity: 71,
-              temp_kf: 2.61,
-            },
-            weather: [
-              {
-                id: 500,
-                main: 'Rain',
-                description: 'light rain',
-                icon: '10n',
-              },
-            ],
-            clouds: {
-              all: 96,
-            },
-            wind: {
-              speed: 1.97,
-              deg: 157,
-              gust: 3.39,
-            },
-            visibility: 10000,
-            pop: 0.33,
-            rain: {
-              '3h': 0.57,
-            },
-            sys: {
-              pod: 'n',
-            },
-            dt_txt: '2022-08-30 18:00:00',
-          },
-          {
-            dt: 1661893200,
-            main: {
-              temp: 292.46,
-              feels_like: 292.54,
-              temp_min: 290.31,
-              temp_max: 292.46,
-              pressure: 1015,
-              sea_level: 1015,
-              grnd_level: 931,
-              humidity: 80,
-              temp_kf: 2.15,
-            },
-            weather: [
-              {
-                id: 500,
-                main: 'Rain',
-                description: 'light rain',
-                icon: '10n',
-              },
-            ],
-            clouds: {
-              all: 68,
-            },
-            wind: {
-              speed: 2.66,
-              deg: 210,
-              gust: 3.58,
-            },
-            visibility: 10000,
-            pop: 0.7,
-            rain: {
-              '3h': 0.49,
-            },
-            sys: {
-              pod: 'n',
-            },
-            dt_txt: '2022-08-30 21:00:00',
-          },
-          {
-            dt: 1662292800,
-            main: {
-              temp: 294.93,
-              feels_like: 294.83,
-              temp_min: 294.93,
-              temp_max: 294.93,
-              pressure: 1018,
-              sea_level: 1018,
-              grnd_level: 935,
-              humidity: 64,
-              temp_kf: 0,
-            },
-            weather: [
-              {
-                id: 804,
-                main: 'Clouds',
-                description: 'overcast clouds',
-                icon: '04d',
-              },
-            ],
-            clouds: {
-              all: 88,
-            },
-            wind: {
-              speed: 1.14,
-              deg: 17,
-              gust: 1.57,
-            },
-            visibility: 10000,
-            pop: 0,
-            sys: {
-              pod: 'd',
-            },
-            dt_txt: '2022-09-04 12:00:00',
-          },
-        ],
-        city: {
-          id: 3163858,
-          name: 'Zocca',
-          coord: {
-            lat,
-            lon,
-          },
-          country: 'IT',
-          population: 4593,
-          timezone: 7200,
-          sunrise: 1661834187,
-          sunset: 1661882248,
-        },
-      }
+    fetch: async (city: CityAlt): Promise<WeatherAlt> => {
+      const request = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${
+          city.lat
+        }&lon=${
+          city.lon
+        }&appid=${
+          import.meta.env.VITE_API_KEY
+        }`
+      )
+      const response: RawWeather = await request.json()
+      const {
+        0: first,
+        8: second,
+        16: third,
+        24: fourth,
+        32: fifth,
+      } = response.list
+
       return {
         city: {
-          id: response.city.id,
-          name: response.city.name,
+          id: city.id,
+          name: city.name,
         },
-        forecast: response.list.map((forcast) => ({
+        forecast: [first, second, third, fourth, fifth].map((forcast) => ({
+          date: new Date(forcast.dt * 1000).toLocaleDateString('es-AR'),
           min: Math.round(forcast.main.temp_min - 273.15),
           max: Math.round(forcast.main.temp_max - 273.15),
         })),
